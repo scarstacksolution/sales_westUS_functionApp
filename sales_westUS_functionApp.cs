59a1e3ef1a63ba99805429f58384f7ef8acff55d
scarstacksolution <techinfo@scarstacksolution.com>
2023-12-08 10:47:36 -0600

    first changes for function app

---
+++ sales_westUS_functionApp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace sales_westUS_functionApp
+{
+    public static class sales_westUS_functionApp
+    {
+        [FunctionName("sales_westUS_functionApp")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. 50 men's wear, 30 female dresses and 40 kids clothing were sold today.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
